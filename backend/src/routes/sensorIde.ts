629174ac46761f7258ce7cc67ba1ee1c7971e40a
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-10-19 20:42:03 +0700

    update sensorIde

--- backend/src/routes/sensorIde.ts
+++ backend/src/routes/sensorIde.ts
@@ -2,6 +2,6 @@ import express from "express"
 import {addSensorData, getSensorData} from "../controllers/sensorController"
 const sensorIdeRouter = express.Router()
 
-sensorIdeRouter.post("/sensor", addSensorData)
+sensorIdeRouter.post("/sensorIde", addSensorData)
 
 export default sensorIdeRouter
