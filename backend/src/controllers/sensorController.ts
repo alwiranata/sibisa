66cde8f63ba3fcf4561d0363ce6aa4d13f1a6070
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-10-22 21:49:51 +0700

    update

--- backend/src/controllers/sensorController.ts
+++ backend/src/controllers/sensorController.ts
@@ -6,12 +6,24 @@ export const getSensorData = async (req: Request, res: Response) => {
 	try {
 		const data = await prisma.sensorData.findMany({
 			orderBy: {createdAt: "desc"},
-			take: 20,
+			take: 5,
 		})
 		res.json(data)
 	} catch (error) {
 		res.status(500).json({
 			message: "Gagal mengambil data sensor",
 		})
+	}
+}
+
+// Untuk export Excel (ambil semua data)
+export const getAllSensorData = async (req: Request, res: Response) => {
+	try {
+		const data = await prisma.sensorData.findMany({
+			orderBy: { createdAt: "desc" },
+		})
+		res.json(data)
+	} catch (error) {
+		res.status(500).json({ message: "Gagal mengambil semua data sensor" })
 	}
 }
--- backend/src/routes/sensorController.ts
+++ backend/src/routes/sensorController.ts
@@ -1,7 +1,8 @@
 import express from "express"
-import {getSensorData} from "../controllers/sensorController"
+import {getSensorData, getAllSensorData} from "../controllers/sensorController"
 const sensorRouter = express.Router()
 
 sensorRouter.get("/sensor", getSensorData)
+sensorRouter.get("/sensorAll", getAllSensorData)
 
 export default sensorRouter
